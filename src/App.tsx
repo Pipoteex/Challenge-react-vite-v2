37e01052efc6d2aa1de55682568f9cc4e0b4a597
Martin <telaf1997@gmail.com>
2025-04-08 15:58:10 -0300

    add: readmev2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import { useState } from "react";
-
 function App() {
-    const [first, setfirst] = useState();
-
     return <div className="text-[100px] flex justify-center items-center h-screen">React + Vite + Tailwind CSS</div>;
 }
 
