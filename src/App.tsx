6bb2c46ea9ffe8bc695f462f3e918250d41205cb
Martin <telaf1997@gmail.com>
2025-04-08 15:42:22 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function App() {
+    const [first, setfirst] = useState();
+
+    return <div className="text-[100px] flex justify-center items-center h-screen">React + Vite + Tailwind CSS</div>;
+}
+
+export default App;
